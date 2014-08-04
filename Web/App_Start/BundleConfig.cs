01063cf1f6c0e2484293f3e8953a783a58ce2fa3
Jacob <jacob.m.celletti@gmail.com>
2014-08-04 16:38:36 -0700

    replace dataTable partial with directives

--- Web/App_Start/BundleConfig.cs
+++ Web/App_Start/BundleConfig.cs
@@ -3,20 +3,20 @@ using System.Web.Optimization;
 
 namespace Web
 {
-    public class BundleConfig
-    {
-        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
-        public static void RegisterBundles(BundleCollection bundles)
-        {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+	public class BundleConfig
+	{
+		// For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+		public static void RegisterBundles(BundleCollection bundles)
+		{
+			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+						"~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
+			bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+						"~/Scripts/jquery-ui-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
+			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+						"~/Scripts/jquery.unobtrusive*",
+						"~/Scripts/jquery.validate*"));
 
 			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
 						"~/Scripts/modernizr-*"));
@@ -24,11 +24,11 @@ namespace Web
 			bundles.Add(new ScriptBundle("~/bundles/Moment").Include("~/Scripts/moment.js"));
 
 			bundles.Add(new ScriptBundle("~/bundles/Angularjs").Include(
-						"~/Scripts/angular.js", "~/Scripts/ui-bootstrap-0.11.0.js"));//.IncludeDirectory("~/Scripts/i18n", "*.js", false));
+						"~/Scripts/angular.js", "~/Scripts/ui-bootstrap-0.11.0.js", "~/Scripts/data.table.js"));
 
 			bundles.Add(new ScriptBundle("~/bundles/Sitejs").Include("~/Scripts/app.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
-        }
-    }
+			bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+		}
+	}
 }
---
+++ Web/Scripts/data.table.js
@@ -0,0 +1,166 @@
+﻿if (!angular) {
+	throw "AngularJs not found";
+}
+
+angular.module("edit.data.table", [])
+.directive("editDataTable", function ($compile) {
+	var template = '<div class="data-grid-container">' +
+	'<div class="header-buttons btn-group" ng-show="headerButtons.length > 0 ">' +
+		'<button type="button" class="btn" ng-repeat="btn in headerButtons" ng-class="btn.className || btn-default" ng-click="btn.click()">{{btn.text}}</button>' +
+	'</div>' +
+	'<table class="data-grid">' +
+		'<thead>' +
+			'<tr>' +
+				'<th ng-repeat="col in columns" ng-click="changeOrder(col.key)">' +
+					'{{col.text}}' +
+				'</th>' +
+				'<th ng-show="actionButtons.length > 0">Actions</th>' +
+			'</tr>' +
+		'</thead>' +
+		'<tbody>' +
+			'<tr ng-repeat="row in rows | orderBy: orderByFunction">' +
+				'<td ng-repeat="col in columns">' +
+					'<div ng-if="!row.isEditing">{{columnFormat(col, row)}}</div>' +
+					'<div ng-if="row.isEditing">' +
+						'<edit-control val="row.data[col.key]" col="col"></edit-control>' +
+					'</div>' +
+				'</td>' +
+				'<td ng-show="actionButtons.length > 0">' +
+					'<div class="btn-group">' +
+						'<button type="button" class="btn" ng-repeat="btn in actionButtons | visibleButtonFilter: row" ng-click="btn.click(row)" ng-class="btn.className || btn-default">{{btn.text}}</button>' +
+					'</div>' +
+				'</td>' +
+			'</tr>' +
+		'</tbody>' +
+	'</table>' +
+'</div>';
+
+	return {
+		restrict: 'AE',
+		transclude: true,
+		scope: {
+			rows: '=',
+			columns: '=',
+			actionButtons: '=',
+			headerButtons: '=',
+			orderByColumn: '=',
+			add: '='
+		},
+		link: function (scope, elm, attrs) {
+			scope.changeOrder = function () {
+				if (scope.orderByColumn === key) {
+					scope.orderByColumn = "-" + key
+				} else {
+					scope.orderByColumn = key
+				}
+			};
+
+			scope.orderByFunction = function (row) {
+				return row.data[scope.orderByColumn];
+			};
+
+			scope.rowEdit = function (column) {
+				return html[column.controlType || "textInput"];
+			};
+
+			scope.columnFormat = function (column, row) {
+				return !!column.format ?
+					column.format(column.formatString, row.data[column.key]) :
+					row.data[column.key]
+			};
+
+			elm.append($compile(template)(scope));
+		}
+	};
+})
+.filter("visibleButtonFilter", function () {
+	return function (buttons, row) {
+		var filtered = []
+		$(buttons).each(function (i, btn) {
+			if (btn.visible(row)) {
+				filtered.push(btn);
+			}
+		});
+		return filtered;
+	};
+})
+.directive("editControl", function ($compile) {
+	var templates = {
+		select: function (scope, row, column) {
+			var select = "<select class='form-control' ng-model='val'>";
+			$.each(column.options, function (key, val) {
+				select += "<option value='" + key + "'>" + val + "</option>";
+			});
+			return select + "</select>"
+		},
+		text: function () { return "<input type='text' class='form-control' ng-model='val' />" },
+		datePicker: function (scope) {
+			scope.dt = moment(scope.val).toDate();
+			scope.dateOptions = {
+				formatYear: 'yyyy',
+			};
+
+			scope.disabled = function (date, mode) {
+				return false;
+			};
+
+			scope.maxDate = moment("01/01/2100").toDate();
+
+			scope.minDate = moment("01/01/2000").toDate();
+
+			scope.opened = false;
+
+			scope.open = function ($event) {
+				$event.preventDefault();
+				$event.stopPropagation();
+
+				scope.opened = true;
+			};
+
+			scope.$watch('dt', function (newValue, oldValue) {
+				scope.val = newValue.toISOString();
+			});
+
+			return '<div class="input-group datepicker-group">' +
+		 ' <input type="text" class="form-control" datepicker-popup="dd-MMMM-yyyy" ng-model="dt" is-open="opened" min-date="minDate" max-date="maxDate" datepicker-options="dateOptions" date-disabled="disabled(date, mode)" ng-required="true" close-text="Close" />' +
+		  '<span class="input-group-btn">' +
+			'<button type="button" class="btn btn-default" ng-click="open($event)"><i class="glyphicon glyphicon-calendar"></i></button>' +
+		  '</span>' +
+		'</div>';
+		},
+	};
+	return {
+		restrict: "AE",
+		link: function (scope, element, attrs) {
+			element.append($compile(templates[scope.col.editingTemplate || "text"](scope))(scope));
+		},
+		transclude: true,
+		scope: {
+			col: "=col",
+			val: "=val"
+		},
+	};
+})
+.directive("calculator", function ($compile) {
+	var template = "<div class='calculator-group input-group'>" +
+		"<textarea type='text' class='form-control' ng-model='val'></textarea>" +
+		"<div class='input-group-addon'>" +
+		"{{evaluated()}}" +
+		"</div>" +
+		"</div>";
+	return {
+		restrict: "E",
+		link: function (scope, element, attrs) {
+			scope.val = null;
+			scope.evaluated = function () {
+				try {
+					return eval(scope.val);
+				} catch (e) {
+					return null;
+				}
+			};
+			element.append($compile(template)(scope));
+		},
+		transclude: true
+	};
+});
